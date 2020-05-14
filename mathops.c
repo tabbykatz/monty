b70422b91b755c33c9a19462d4c37fdf1aa281b8
tabbykatz <tomelay@gmail.com>
2020-05-14 15:14:19 +0000

    fix div for short && empty stacks

--- mathops.c
+++ mathops.c
@@ -59,6 +59,13 @@ void div_op(stack_t **stack, __attribute__((unused))unsigned int linenumber)
 {
 	stack_t *freeable;
 
+	if (!*stack || !(*stack)->next)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't div, stack too short\n"
+				, monty.line_number);
+		free_it_all();
+		exit(EXIT_FAILURE);
+	}
 	if ((*stack)->n == 0)
 	{
 		dprintf(STDERR_FILENO, "L%u: division by zero\n", monty.line_number);
@@ -73,13 +80,6 @@ void div_op(stack_t **stack, __attribute__((unused))unsigned int linenumber)
 		*stack = (*stack)->next;
 		(*stack)->prev = NULL;
 		free(freeable);
-	}
-	else
-	{
-		dprintf(STDERR_FILENO, "L%u: can't div, stack too short\n",
-				monty.line_number);
-		free_it_all();
-		exit(EXIT_FAILURE);
 	}
 }
 /**
